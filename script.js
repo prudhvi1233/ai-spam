293ac880cc6186c4d10fd6f7e5419f9ac28a55e2
Prudhvi Behara <153195613+prudhvi1233@users.noreply.github.com>
2026-02-13 11:07:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const joker = document.getElementById("joker");
 
+let angle = 0;
+
+function moveJoker(){
+    angle += 0.05;
+    const x = Math.sin(angle) * 30;  // left-right movement
+    const y = Math.cos(angle) * 20;  // up-down movement
+    joker.style.transform = `translate(${x}px, ${y}px)`;
+    requestAnimationFrame(moveJoker);
+}
+
+moveJoker();
